6f4c86989bc82658bfceadbc60299bc8745885c6
jguimeram <jguimera54@gmail.com>
2024-05-15 16:58:58 +0200

    update layout

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -13,9 +13,13 @@
 <body>
     <section class="vh-100" style="background-color: #eee;">
         <div class="container py-5 h-100">
+            <!-- div applies flex: -->
             <div class="row d-flex justify-content-center align-items-center h-100">
+                <!-- div applies columns sizes: -->
                 <div class="col col-lg-9 col-xl-7">
+                    <!-- parent card div -->
                     <div class="card rounded-3">
+                        <!-- inner card div -->
                         <div class="card-body p-4">
 
                             <h4 class="text-center my-3 pb-3">To Do App</h4>
@@ -27,18 +31,18 @@
                                         <input type="text" id="form1" class="form-control" />
                                     </div>
                                 </div>
-                                <div class="col-12">
+                                <div class="col-12 mt-3">
                                     <label class="form-label" for="form1">Description</label>
                                     <textarea id="form1" class="form-control"></textarea>
                                 </div>
-                                <div class="col-12">
+                                <div class="col-12 mt-3">
                                     <div data-mdb-input-init class="form-outline">
                                         <label class="form-label" for="form1">Select a due date:</label>
                                         <input type="date" id="form1" class="form-control" />
                                     </div>
                                 </div>
 
-                                <div class="col-12 mt-1">
+                                <div class="col-12 mt-3">
                                     <button type="submit" data-mdb-button-init data-mdb-ripple-init
                                         class="btn btn-primary">Add</button>
                                 </div>
@@ -56,14 +60,14 @@
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <th scope="row">1</th>
+                                        <th scope="row"><a href="#">1</a></th>
                                         <td>Buy groceries for next week</td>
                                         <td>In progress</td>
                                         <td>
                                             <button type="submit" data-mdb-button-init data-mdb-ripple-init
-                                                class="btn btn-danger">Delete</button>
+                                                class="btn btn-danger p-1">Delete</button>
                                             <button type="submit" data-mdb-button-init data-mdb-ripple-init
-                                                class="btn btn-success ms-1">Finished</button>
+                                                class="btn btn-success p-1 ms-3">Finished</button>
                                         </td>
                                         <td>15/05/2024</td>
                                     </tr>
